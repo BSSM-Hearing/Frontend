605ad56f85b19c08992ad3c74119b192625f0208
LLTae2 <taehyun5820@gmail.com>
2023-01-12 01:32:23 +0900

    Update::전화모드 페이지 퍼블리싱

--- src/pages/callMode/Index.js
+++ src/pages/callMode/Index.js
@@ -4,13 +4,30 @@ import * as S from "./Style";
 import { IoMdCall } from "react-icons/io";
 
 const Index = () => {
+  const [isCalling, setIsCalling] = useState(false);
   return (
     <Frame>
       <S.CallModeContainer>
         <S.Title>전화 모드</S.Title>
-        <S.CallBtn>
-          <IoMdCall color={"white"} size={80} />
-        </S.CallBtn>
+        {!isCalling ? (
+          <>
+            <S.CallBtn bgColor={"#1c69ff"} onClick={() => setIsCalling(true)}>
+              <IoMdCall color={"white"} size={80} />
+            </S.CallBtn>
+          </>
+        ) : (
+          <S.CallingBox>
+            <S.DialogBox>
+              <S.LeftDialog bgColor={"whitesmoke"}>안녕하세여</S.LeftDialog>
+              <S.RightDialog bgColor={"#1c69ff"}>안녕하세여</S.RightDialog>
+              <S.LeftDialog bgColor={"whitesmoke"}>안녕하세여</S.LeftDialog>
+              <S.RightDialog bgColor={"#1c69ff"}>안녕하세여</S.RightDialog>
+            </S.DialogBox>
+            <S.CallBtn onClick={() => setIsCalling(false)} bgColor={"red"}>
+              종료
+            </S.CallBtn>
+          </S.CallingBox>
+        )}
       </S.CallModeContainer>
     </Frame>
   );
--- src/pages/callMode/Style.js
+++ src/pages/callMode/Style.js
@@ -9,7 +9,7 @@ export const CallModeContainer = styled.div`
 `;
 export const Title = styled.p`
   position: absolute;
-  top: 2%;
+  top: 1%;
   text-align: center;
   font-size: 3.5rem;
   font-weight: 700;
@@ -17,8 +17,42 @@ export const Title = styled.p`
 `;
 export const CallBtn = styled.button`
   width: 40%;
-  height: 25%;
-  background-color: #1c69ff;
+  height: calc(90vh / 4);
+  background-color: ${(props) => props.bgColor};
   border-radius: 300px;
   border: none;
+  font-size: 3.5rem;
+  font-weight: 700;
+  color: white;
+`;
+
+export const CallingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 8%;
+  height: 75%;
+  width: 100%;
+`;
+export const DialogBox = styled.div`
+  width: 100%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const LeftDialog = styled.div`
+  width: 60%;
+  height: 16%;
+  background-color: ${(props) => props.bgColor};
+  box-shadow: 1px 4px 8px 2px #aaa;
+  border-radius: 15px;
+  margin-left: 5%;
+  padding: 2%;
+  font-size: 2rem;
+`;
+export const RightDialog = styled(LeftDialog)`
+  margin-left: 30%;
+  color: white;
 `;
