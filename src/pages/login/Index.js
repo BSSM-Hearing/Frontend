259da95e6e25406f5922eaec9110b67bfb2760af
LLTae2 <taehyun5820@gmail.com>
2023-01-11 15:59:10 +0900

    Update::로그인 페이지 퍼블리싱

--- src/pages/login/Index.js
+++ src/pages/login/Index.js
@@ -8,12 +8,16 @@ const Index = () => {
     <Frame>
       {/* <S.LoginForm>asd</S.LoginForm> */}
       <S.LoginContainer>
-        <S.TopBox></S.TopBox>
+        <S.TopBox>로그인</S.TopBox>
         <S.LoginForm>
           <S.Input />
           <S.Input />
           <S.SubmitBtn>로그인</S.SubmitBtn>
         </S.LoginForm>
+        <S.BottomBox>
+          <S.Link>회원가입</S.Link>
+          <S.Link>비밀번호 찾기</S.Link>
+        </S.BottomBox>
       </S.LoginContainer>
     </Frame>
   );
--- src/pages/login/Style.js
+++ src/pages/login/Style.js
@@ -2,9 +2,7 @@ import styled from "styled-components";
 
 export const LoginContainer = styled.div`
   width: 100%;
-  height: 90%;
-  background-color: black;
-  /* background-color: black; */
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -22,14 +20,20 @@ export const LoginForm = styled.div`
 `;
 
 export const TopBox = styled.div`
-  width: 100%;
-  height: 40%;
+  width: 170%;
+  height: 30%;
   background-color: #1c69ff;
+  border-end-start-radius: 50%;
+  border-end-end-radius: 50%;
+  font-size: 7.2rem;
+  color: white;
+  text-align: center;
+  line-height: 270%;
 `;
 
 export const Input = styled.input`
   width: 100%;
-  height: 20%;
+  height: 23%;
   background-color: white;
   border-radius: 100px;
   border: 2px solid blue;
@@ -38,10 +42,21 @@ export const Input = styled.input`
 
 export const SubmitBtn = styled.button`
   width: 100%;
-  height: 20%;
+  height: 23%;
   background-color: #1c69ff;
   border-radius: 100px;
   border: none;
   font-size: 3.5rem;
   color: white;
+`;
+
+export const BottomBox = styled.div`
+  width: 100%;
+  height: 30%;
+`;
+
+export const Link = styled.p`
+  color: #1c69ff;
+  font-size: 2rem;
+  text-align: center;
 `;
