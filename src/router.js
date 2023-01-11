5413db36154343874094b0278313f96fd9ebbc09
J1min <aoiharu41@gmail.com>
2023-01-11 09:46:16 +0900

    Feat: add speech kit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Test from "./pages/Test";
+import Speech from "./components/speech";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/speech" element={<Speech />} />
       </Routes>
     </BrowserRouter>
   );
